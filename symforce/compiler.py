f8d2f12c9ab8ab7fd2a12ca33d4f317366e637d0
Asa Hammond <asa@automaton.is>
2024-02-21 19:50:26 -0800

    updated compiler, add python lib always

--- symforce/compiler.py
+++ symforce/compiler.py
@@ -23,46 +23,55 @@ def cli():
     pass
 
 @cli.command()
+@click.option('--basename',  type=str, help = "base name for residual function, and factor")
 @click.option('--input_path',  help="input symforce python file")
-@click.option('--function',  type=str, help = "which function name should we compile")
+@click.option('--function_name',  type=str, help = "which function name should we compile")
 @click.option('--function_is_generator', is_flag=True, help="is the function a function generator?")
 @click.option('--arguments',  multiple=True, help="which arguments of the function should we linearize around")
 @click.option('--output_names',  multiple=True, help="output the names of the arguments")
 @click.option('--output_function_path', help="output path for function (eg residual.h ) file")
 @click.option('--output_factor_path', default = None, help="output path for factor.h file")
-def compile(input_path, function, function_is_generator, arguments, output_names, output_function_path, output_factor_path):
+def compile(basename, input_path, function_name, function_is_generator, arguments, output_names, output_function_path, output_factor_path):
     import sympy
 
-    print("Starting symforce codegen from {}", input_path)
+    print("Starting symforce codegen from {}".format(input_path))
     # read the python file from disk and execute it so the function is in the global namespace
     exec(open(input_path,"r").read(), globals())
-
+    # get the function from the global namespace
+    FUNC = globals()[function_name]
+    
     data = None
+    input_types = None
 
     if(function_is_generator):
-        print("function is generator, calling it!!!!!!!!!!!!!!!!!!!!")
-        #function = function()
-        globals()[function] = globals()[function]()
+        input_types, FUNC = FUNC()
+
+    if output_factor_path:
+        # if this is a factor the base function is FooFactorResidual
+        # do that by appending snake case
+        basename = basename + "_residual"
 
     if(output_names):
-        data = codegen.Codegen.function(globals()[function], 
+        data = codegen.Codegen.function(FUNC, 
                                     config=codegen.CppConfig(),
-                                    name = function,
+                                    name = basename,
+                                    input_types = input_types,
                                     output_names = output_names
                                     )
     else:
-        data = codegen.Codegen.function(globals()[function], 
+        data = codegen.Codegen.function(FUNC, 
                                     config=codegen.CppConfig(),
-                                    name = function,
+                                    name = basename,
+                                    input_types = input_types,
                                     )
 
     # generate the residual 
     generated_function = data.generate_function()
     open(output_function_path,"w").write(open(generated_function.generated_files[0]).read())
-    print("function: {}".format(function))
-    print("to {}".format(function, output_function_path))
+    print("function: {} -> codegen with base name: {}".format(function_name, basename))
 
     if output_factor_path:
+        #basename_residual -> basename_factor when we output a factor
         # also generate the linearization function (eg the factor)
         # and write it to disk
         codegen_linearization = data.with_linearization(
@@ -71,7 +80,11 @@ def compile(input_path, function, function_is_generator, arguments, output_names
 
         metadata_factor = codegen_linearization.generate_function()
         open(output_factor_path,"w").write(open(metadata_factor.generated_files[0]).read())
-        print("factor:   {} to {}".format(function, output_factor_path))
+        print("function: {}".format(output_function_path))
+        print("factor:   {}".format(output_factor_path))
+    else:
+        # this mode is just building a codegen py -> cc function. this is the path the residual function takes above
+        print("function: {}".format(output_function_path))
 
 if __name__ == "__main__":
     cli()
